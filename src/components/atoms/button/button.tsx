8196aea9c4709941b97abf8f2e38c1059bafa3e0
debabin <dmitriybabinup@gmail.com>
2026-08-19 22:11:29 +0700

    main 🧊 v1.0.6

--- src/components/atoms/button/button.tsx
+++ src/components/atoms/button/button.tsx
@@ -1,30 +1,43 @@
+import type { VariantProps } from 'class-variance-authority';
 import type { ComponentProps } from 'react';
 
 import { cn } from '@siberiacancode/reactuse';
+import { cva } from 'class-variance-authority';
 import { Slot } from 'radix-ui';
 
 import styles from './button.module.css';
 
-export type ButtonVariant = 'ghost' | 'link' | 'outline' | 'primary' | 'secondary';
-export type ButtonSize = 'lg' | 'md' | 'sm';
+const buttonVariants = cva(styles.button, {
+  variants: {
+    variant: {
+      primary: styles.primary,
+      secondary: styles.secondary,
+      outline: styles.outline,
+      ghost: styles.ghost,
+      link: styles.link
+    },
+    size: {
+      lg: styles.lg,
+      md: styles.md,
+      sm: styles.sm
+    }
+  },
+  defaultVariants: {
+    variant: 'primary',
+    size: 'lg'
+  }
+});
 
-export interface ButtonProps extends ComponentProps<'button'> {
+export interface ButtonProps extends ComponentProps<'button'>, VariantProps<typeof buttonVariants> {
   asChild?: boolean;
-  size?: ButtonSize;
-  variant?: ButtonVariant;
 }
 
-export const Button = ({
-  className,
-  variant = 'primary',
-  size = 'lg',
-  asChild = false,
-  ...props
-}: ButtonProps) => {
+export const Button = ({ className, variant, size, asChild = false, ...props }: ButtonProps) => {
   const Comp = asChild ? Slot.Root : 'button';
+
   return (
     <Comp
-      className={cn(styles.button, styles[variant], styles[size], className)}
+      className={cn(buttonVariants({ variant, size }), className)}
       data-size={size}
       data-slot='button'
       data-variant={variant}
--- src/components/atoms/typography/typography.tsx
+++ src/components/atoms/typography/typography.tsx
@@ -1,9 +1,41 @@
 import type { ComponentProps } from 'react';
 
 import { cn } from '@siberiacancode/reactuse';
+import { cva } from 'class-variance-authority';
 import { Slot } from 'radix-ui';
 
 import styles from './typography.module.css';
+
+export const typographyVariants = cva(styles.typography, {
+  variants: {
+    variant: {
+      display: styles.display,
+
+      'heading-2xl': styles.heading2xl,
+      'heading-xl': styles.headingXl,
+      'heading-lg': styles.headingLg,
+      'heading-md': styles.headingMd,
+
+      'title-lg': styles.titleLg,
+      'title-md': styles.titleMd,
+
+      'body-lg': styles.bodyLg,
+      'body-md': styles.bodyMd,
+      'body-sm': styles.bodySm,
+
+      link: styles.link,
+      caption: styles.caption
+    },
+    pixelify: {
+      true: styles.pixelify,
+      false: null
+    }
+  },
+  defaultVariants: {
+    variant: 'body-md',
+    pixelify: false
+  }
+});
 
 export type TypographyTag = 'a' | 'div' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span';
 
